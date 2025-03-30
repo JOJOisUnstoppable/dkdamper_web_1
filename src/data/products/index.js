815413a8ff430bb7a2c518bffc64d57ab6f3f696
sam <samlin1995@foxmail.com>
2025-03-30 11:10:05 +0800

    feat: add PR-L241 high-force aluminum damper product data

--- src/data/products/index.js
+++ src/data/products/index.js
@@ -2,12 +2,14 @@ import { productData as JP_CA1210 } from './JP-CA1210'
 import { productData as PR_L202 } from './PR-L202'
 import { productData as PR_L208 } from './PR-L208'
 import { productData as PR_L223 } from './PR-L223'
+import { productData as PR_L241 } from './PR-L241'
 
 const productsData = {
   'JP-CA1210': JP_CA1210,
   'PR-L202': PR_L202,
   'PR-L208': PR_L208,
-  'PR-L223': PR_L223
+  'PR-L223': PR_L223,
+  'PR-L241': PR_L241
 }
 
 export const getProductById = (id) => {
